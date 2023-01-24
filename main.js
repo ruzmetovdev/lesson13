bdb6bde01974ad7b46ddd14151839d9ae937b7bb
ruzmetovdev <boburruzmetov2006@gmail.com>
2023-01-25 03:58:29 +0500

    second

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 
 setTimeout(() => {
   document.getElementById("loading").style.display = "none";
-}, 1000);
+}, 500);
 
-window.addEventListener("scroll", function () {
-  toggleBacktop();
-});
+// back to top//
 
-let backtop = document.getElementById("backtop");
 
-function toggleBacktop() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    backtop.style.opacity = 1;
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    backtop.style.opacity = 0;
+    mybutton.style.display = "none";
   }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// dark mode//
+
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "./images/oy.svg";
+  } else {
+    this.firstElementChild.src = "./images/oy.svg";
+  }
+  document.body.classList.toggle("light");
+});
